f8a2a527c490b2cb55483d347ec791dc0f60c4c0
yuhanzhang <yuhanzhang@localhost>
2025-01-21 17:03:19 -0600

    make the robot draw a picture of tree

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,34 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(100);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(150);
+		robot.turnLeft(135);
+		robot.forward(120);
+		robot.turnRight(135);
+		robot.forward(60);
+		robot.turnLeft(135);
+		robot.forward(130);
+		robot.turnRight(135);
+		robot.forward(60);
+		robot.turnLeft(135);
+		robot.forward(180);
+		
+		robot.turnLeft(90);
+		robot.forward(180);
+		robot.turnLeft(135);
+		robot.forward(60);
+		robot.turnRight(135);
+		robot.forward(130);
+		robot.turnLeft(135);
+		robot.forward(60);
+		robot.turnRight(135);
+		robot.forward(120);
+		robot.turnLeft(135);
+		robot.forward(150);
+		robot.turnRight(90);
+		robot.forward(120);
 	}
 
 }
